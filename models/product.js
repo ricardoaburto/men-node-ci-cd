4d638ede805a5ddb9d9e41132258adcaca239556
Ricardo Aburto Jara <r.ricardo.aburtojara@gmail.com>
2022-04-18 11:33:00 -0400

    Init project and ci-cd

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: { type: String },
+  description: { type: String },
+  price: { type: Number },
+  inStock: { type: Boolean }
+});
+
+module.exports = mongoose.model("product",productSchema);
---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { type: String, required: true, min: 6, max: 255 },
+  email: { type: String, required: true, min: 6, max: 255 },
+  password: { type: String, required: true, min: 6, max: 255 },
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("user",UserSchema);
