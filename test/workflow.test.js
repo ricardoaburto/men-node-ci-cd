8f2d49b935ce5ce52e8d138591c1384ada1488b7
Ricardo Aburto Jara <r.ricardo.aburtojara@gmail.com>
2022-04-18 11:58:41 -0400

    set timeout

--- test/workflow.test.js
+++ test/workflow.test.js
@@ -7,6 +7,7 @@ chai.use(chaiHttp);
 
 describe("User workflow tests", () => {
   it("should register + login a user, create product and verify 1 in DB", (done) => {
+    this.timeout(10000);
     // 1) Register new user
     let user = {
       name: "Peter Petersen",
@@ -83,6 +84,7 @@ describe("User workflow tests", () => {
   });
 
   it("should register + login a user, create product and delete it from DB", (done) => {
+    this.timeout(10000);
     // 1) Register new user
     let user = {
       name: "Peter Petersen",
